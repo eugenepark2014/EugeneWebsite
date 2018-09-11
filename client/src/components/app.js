a4ad3848541fc012bd743a02a9348cd85238db4f
eugenepark2014 <eugenepark2014@gmail.com>
2018-09-11 19:55:37 -0400

    first commit

---
+++ client/src/components/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomePage from "./body/homePage.js";
+import NavBar from "./header/navBar.js";
+import Footer from "./footer/footer.js";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Route name="home" exact path="/" component={HomePage} />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          <h1> this is the footer component</h1>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
